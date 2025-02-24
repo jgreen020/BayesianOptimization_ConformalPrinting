419b1e8c7f48648c3a281265303d80f6dcba8084
jgreen020 <jakegcolwell@gmail.com>
2025-02-24 14:47:52 -0500

    parallel computing attempt 2

--- Inputs.m
+++ Inputs.m
@@ -16,18 +16,19 @@ SurfB='Surf1a.csv'; % Name of the file containing the CT scan data
 Curve=@ArchSpiral; % Curve Parameterizaion, Inputs: t (scalar or vector in [0,1]), Outputs: u,v (scalars or vectors representing a point in U)
 
 % Surface Fitting Settings
-method=1; % Which surface fitting method to use. 
+method=2; % Which surface fitting method to use. 
           % 1) Bézier Surface Fitting
           % 2) Naive Bayesian Optimization
           % 3) Bayesian Optimization with prior trained on SurfA
 doaprint=false; % Is TCS being performed (true) or is this a simulation only (false)
+savedata=false; % Save data from the run
 if method==1
     n=3; % Grid size of sampling points for Bezier Fitting (n x n grid, n > 3)
-    m=12; % If doing a simulation, program will test bezier fitting for n:1:m sequencially
+    m=8; % If doing a simulation, program will test bezier fitting for n:1:m in parallel
 elseif method==2 || method==3
     n=13; % Number of points to sample before starting Bayesian Optimization 
     % ^(DO NOT CHANGE)
-    m=225; % Final number of points for Bayseian Optimization
+    m=27; % Final number of points for Bayseian Optimization
 else
     disp('Invalid Method Number')
     return
@@ -38,9 +39,9 @@ cmax=0.165; % Maximum Deviation (mm)
 
 %% Importing Specified Surfaces
 % Data import for Surfaces
-dataA = readtable(SurfA);
+dataA = readtable(strcat('Data/CT Scans/',SurfA));
 dataA.Properties.VariableNames={'x','y','z'};
-dataB = readtable(SurfB);
+dataB = readtable(strcat('Data/CT Scans/',SurfB));
 dataB.Properties.VariableNames={'x','y','z'};
 
 % Grabbing only data in the range specified by pad
