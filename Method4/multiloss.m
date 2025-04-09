c4e0820e34b1c7b6919cb189e671857d154e306b
jgreen020 <jakegcolwell@gmail.com>
2025-04-09 14:27:53 -0400

    beginning to implement method 4
    makeNetwork.mlx
    - Generates a convolutional auto-encoder network
    
    multiloss.m
    - Loss function for neural network training to include multi-scale supervision
    - currently blank
    
    trainNetwork.m
    - Train the network generated by makeNetwork.mlx
    - Cannot work until multiloss.m is completed

---
+++ Method4/multiloss.m
@@ -0,0 +1,14 @@
+function val = multiloss(Y,T)
+% Evaluate multiscale monitoring loss.
+
+% Inputs:
+%          Y - Formatted dlarray of predictions
+%          T - Formatted dlarray of targets
+%
+% Outputs:
+%           val - Metric value
+%
+% Define the metric function here.
+
+
+end
